b6f9772f98d8aa886e3d4e01f4c167e21090c3b6
luxas12300 <costalucas017@gmail.com>
2025-07-23 08:58:03 -0300

    Creating user's logic + units tests with NextJS

---
+++ daily-diet/src/modules/users/entities/User.ts
@@ -0,0 +1,44 @@
+import { randomUUID } from "crypto";
+
+interface UserSchema{
+    name: string;
+    email: string;
+    password: string;
+}
+
+export class User{
+    props: UserSchema;
+    _id: string;
+
+    constructor(props: UserSchema, id?: string){
+        this.props = props;
+        this._id = id || randomUUID();
+    }
+
+   getId(): string{
+    return this._id;
+   } 
+   getEmail(): string{
+    return this.props.email;
+   }
+
+   setEmail(email: string){
+    this.props.email = email;
+   }
+
+   getName() : string{
+    return this.props.name;
+   }
+
+   setName(name: string) {
+    this.props.name = name;
+   }
+
+   getPassword(): string{
+    return this.props.password;
+   }
+
+   setPassword(password: string){
+    this.props.password = password;
+   }
+}
---
+++ daily-diet/src/modules/users/entities/useCases/createUserUseCase/CreateUserUseCase.spec.ts
@@ -0,0 +1,42 @@
+import { compare } from "bcrypt"
+import { UserRepository } from "../../repositories/UserRepository"
+import { UserRepositoryInMemory } from "../../repositories/UserRepositoryInMemory"
+import { CreateUserUseCase } from "./CreateUserUseCase"
+
+
+let createUserUseCase : CreateUserUseCase
+let userRepositoryInMemory: UserRepositoryInMemory
+
+describe('Create User', () => {
+    beforeEach(() =>{
+        userRepositoryInMemory = new UserRepositoryInMemory()
+        createUserUseCase = new CreateUserUseCase(userRepositoryInMemory)
+    })
+
+    it('Should be able to create user', async () =>{
+        expect(userRepositoryInMemory.users).toEqual([]);
+
+        const user = await createUserUseCase.execute({
+            email: 'email@email.com',
+            name: 'Lucas',
+            password: '12345',
+        });
+
+        expect(userRepositoryInMemory.users).toEqual([user])
+    })
+
+    it('Should be able to create user whith password encrypted', async () =>{
+        const userPasswordWithoutEncription = "12345";
+
+        const user = await createUserUseCase.execute({
+            email: 'email@email.com',
+            name: 'Lucas',
+            password: userPasswordWithoutEncription,
+        });
+    
+
+        const userHasPasswordEncrypted = await compare(userPasswordWithoutEncription, user.getPassword())
+
+        expect(userHasPasswordEncrypted).toBeTruthy()
+    })
+})
